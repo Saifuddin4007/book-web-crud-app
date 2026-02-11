0510a95d0f6898d77058e0608bff12df19fa4689
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-02-11 12:34:00 +0530

    Final Additions

--- client/src/routes/CreateBook.jsx
+++ client/src/routes/CreateBook.jsx
@@ -1,26 +1,87 @@
 import React, { useState } from 'react'
 import NoImage from '/no-image-selected.jpg'
+import { Link } from 'react-router-dom'
 
 const CreateBook = () => {
     const [title, setTitle] = useState("");
     const [slug, setSlug] = useState("");
     const [description, setDescription] = useState("");
-    const [category, setCategory] = useState("");
+    const [category, setCategory] = useState([]);
     const [stars, setStars] = useState(0);
+    const [thumbnail, setThumbnail]= useState(null);
+    const [submitted, setSubmitted]= useState(false);
+    const [image, setImage]= useState(null);
+
+
+    const createBook= async (e)=>{
+        e.preventDefault();
+
+        const formData= new FormData();
+        formData.append('title', title);
+        formData.append('slug', slug);
+        formData.append('description', description);
+        formData.append('category', category);
+        formData.append('stars', stars);
+        formData.append('thumbnail', thumbnail);
+
+
+        try{
+            const response= await fetch('http://localhost:3000/api/books', {
+                method:'POST',
+                body: formData
+            });
+
+            if(response.ok){
+                setTitle("");
+                setSlug("");
+                setDescription("");
+                setCategory("");
+                setStars(0);
+                setThumbnail(null);
+                setSubmitted(true);
+                alert("Book created successfully");
+                console.log("Book reated successfully");
+            }
+        }catch(err){
+            console.error(err);
+        }
+    }
+
+
+    const handleImage=(e)=>{
+        if(e.target.files && e.target.files[0]){
+            setImage(URL.createObjectURL(e.target.files[0]));
+            setThumbnail(e.target.files[0]);
+        }
+    }
+
+    const handleCategory= (e)=>{
+        setCategory(e.target.value.split(',').map(cat => cat.trim()));
+    }
    
 
 
     return (
         <>
+            <Link to={'/books'}>👈Back to Books</Link>
+
             <h1>Create Book</h1>
 
             <p>Here You can donate books for your readers for free</p>
 
-            <form action="" className="bookdetails">
+            {submitted?(
+                <h2>Book created successfully!</h2>
+            ):(
+                <form action="" className="bookdetails" onSubmit={createBook}>
                 <div className='col-1'>
                     <label htmlFor="">Upload Thumbnail</label>
-                    <img src={NoImage} alt="No-Image-Selected" />
-                    <input type="file" accept='image/gif, image/jpg, image/jpeg, image/png' />
+                    <img src={image || NoImage} alt="Image preview" />
+                    <input 
+                    type="file" 
+                    accept='image/gif, image/jpg, image/jpeg, image/png' 
+                    name='thumbnail'
+                    onChange={handleImage}
+                    />
 
 
                 </div>
@@ -49,7 +110,7 @@ const CreateBook = () => {
                         <label htmlFor="">Category</label>
                         <input type="text" 
                         value={category} 
-                        onChange={(e) => setCategory(e.target.value)} />
+                        onChange={() => setCategory(handleCategory)} />
                     </div>
                     <div>
                         <label htmlFor="">Stars</label>
@@ -65,6 +126,9 @@ const CreateBook = () => {
 
                 </div>
             </form>
+            )}
+
+            
 
 
         </>
